319fccb1ce2e2f3ba8982386a773d0228c583096
Entity <ruslan.kuzma@swanteams.com>
2012-07-21 20:10:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopScaffold::Application.config.secret_token = '652ac8326c48d0284f82d3a55cb4e85b04e37393053a9500d9b148d440e843716d2e99899ba1c4cd7e8c40d139aa84e969ab0f88b61cb1e1e003c2dd294f45c5'
