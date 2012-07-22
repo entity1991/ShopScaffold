188d2fd0309c68cb7bb1b08b1f0948a5eff25a90
Entity <ruslan.kuzma@swanteams.com>
2012-07-22 22:38:11 +0300

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,15 +20,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, :notice => 'Article was successfully created.' }
-        format.json { render :json => @user, :status => :created, :location => @article }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @user.errors, :status => :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "Welcome to Shop!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
     end
   end
 
